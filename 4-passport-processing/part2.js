79f45f81dbf656abeafc684dfd73954ea331808a
Andrea Arias <ariasdrea@gmail.com>
2020-12-04 23:16:39 +0100

    :zap:

--- 4-passport-processing/part2.js
+++ 4-passport-processing/part2.js
@@ -1,20 +1,22 @@
 const fs = require("fs");
 
-let passports = fs
+let allPassports = fs
     .readFileSync(__dirname + "/passports.txt", "utf8")
     .split("\n\n");
 
 let validPassports = 0;
 let maybePassports = 0;
-let requiresExtraValidation = [];
+let dataRequiringExtraValidation = [];
+
 function countingPassports() {
-    for (var i = 0; i < passports.length; i++) {
-        let data = passports[i].replace(/\n/gi, " ");
+    for (var i = 0; i < allPassports.length; i++) {
+        let data = allPassports[i].replace(/\n/gi, " ");
+
         if (data.match(/:/g).length === 8) {
-            requiresExtraValidation.push(data);
+            dataRequiringExtraValidation.push(data);
             validPassports++;
         } else if (!data.match(/cid/g) && data.match(/:/g).length === 7) {
-            requiresExtraValidation.push(data);
+            dataRequiringExtraValidation.push(data);
             maybePassports++;
         }
     }
@@ -22,18 +24,19 @@ function countingPassports() {
     return validPassports + maybePassports;
 }
 
-let passportsToValidate = countingPassports();
+let numPotentialValidPassports = countingPassports();
 
 let arr = [];
 let eyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+let numFinalValidPassports;
 
-function validatePassports(data, passportsToValidate) {
+function validatePassports(data, num) {
     for (var i = 0; i < data.length; i++) {
-        let arrOfPasswords = data[i].split(" ");
+        let arrOfPassports = data[i].split(" ");
 
         var obj = {};
-        for (var j = 0; j < arrOfPasswords.length; j++) {
-            var split = arrOfPasswords[j].split(":");
+        for (var j = 0; j < arrOfPassports.length; j++) {
+            var split = arrOfPassports[j].split(":");
             obj[split[0].trim()] = split[1].trim();
         }
 
@@ -47,31 +50,33 @@ function validatePassports(data, passportsToValidate) {
         let inches = hgt.substring(0, hgt.length - 2);
 
         if (byr.length === 4 && !(byr >= 1920 && byr <= 2002)) {
-            passportsToValidate--;
+            num--;
         } else if (iyr.length == 4 && !(iyr >= 2010 && iyr <= 2020)) {
-            passportsToValidate--;
+            num--;
         } else if (eyr.length == 4 && !(eyr >= 2020 && eyr <= 2030)) {
-            passportsToValidate--;
+            num--;
         } else if (hgt.indexOf("cm") === -1 && hgt.indexOf("in") === -1) {
-            passportsToValidate--;
+            num--;
         } else if (hgt.indexOf("cm") > 0 && !(cm >= 150 && cm <= 193)) {
-            passportsToValidate--;
+            num--;
         } else if (hgt.indexOf("in") > 0 && !(inches >= 59 && inches <= 76)) {
-            passportsToValidate--;
+            num--;
         } else if (hcl.indexOf("#") !== 0 || hcl.length != 7) {
-            passportsToValidate--;
+            num--;
         } else if (!hcl.match(/#[a-f0-9]+/)) {
-            passportsToValidate--;
+            num--;
         } else if (pid[0] === 0 || pid.length != 9) {
-            passportsToValidate--;
+            num--;
         } else {
             if (eyeColors.indexOf(ecl) === -1) {
-                passportsToValidate--;
+                num--;
             }
         }
     }
 
-    return passportsToValidate;
+    return (numFinalValidPassports = num);
 }
 
-console.log(validatePassports(requiresExtraValidation, passportsToValidate));
+console.log(
+    validatePassports(dataRequiringExtraValidation, numPotentialValidPassports)
+);
