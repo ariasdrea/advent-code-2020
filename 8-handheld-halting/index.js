02cff9bf357827ee21018fc92e7a2bfb4f2faa5b
Andrea Arias <ariasdrea@gmail.com>
2020-12-08 09:41:17 +0100

    gimme that accumulator

---
+++ 8-handheld-halting/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+let instructions = fs
+    .readFileSync(__dirname + "/instructions.txt", "utf8")
+    .split("\n");
+
+function cleanData(data) {
+    let acc = 0;
+    let i = 0;
+    let executedMoves = [];
+
+    while (true) {
+        let move = data[i].slice(0, 3);
+        let entireNum = parseInt(data[i].substring(4));
+        executedMoves.push(i);
+
+        if (move == "acc") {
+            acc += entireNum;
+            i++;
+        }
+
+        if (move == "jmp") {
+            i += entireNum;
+        }
+
+        if (move == "nop") {
+            i++;
+        }
+
+        if (executedMoves.indexOf(i) !== -1) {
+            return acc;
+        }
+    }
+}
+
+console.log(cleanData(instructions));
